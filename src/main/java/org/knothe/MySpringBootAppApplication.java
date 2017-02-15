b54bdfc47df9e31acdefe1e03411cb8b38e7b857
arturk94 <dev@knothe.org>
2017-02-15 20:54:32 +0100

    First commit
    First commit

---
+++ src/main/java/org/knothe/MySpringBootAppApplication.java
@@ -0,0 +1,12 @@
+package org.knothe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAppApplication.class, args);
+	}
+}
